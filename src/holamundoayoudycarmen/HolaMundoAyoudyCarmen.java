085af339729dae88e4eee6721f90130fed38951f
a022572787n <a022572787n@S09-PC12>
2017-11-10 19:33:06 +0100

    Prueba 1

---
+++ src/holamundoayoudycarmen/HolaMundoAyoudyCarmen.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundoayoudycarmen;
+
+/**
+ *
+ * @author a022572787n
+ */
+public class HolaMundoAyoudyCarmen {
+public static void main(String[] args) {
+System.out.println("Hola, Mundo!!!!"); 
+    /**
+     * @param args the command line arguments
+     */
+     
+        // TODO code application logic here
+    
+    
+}
+}
